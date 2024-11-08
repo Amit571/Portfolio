3f35bb89bf461dbfce6651b5c11a82baeb1ff63a
Amit Shinde <amitshinde44020@gmail.com>
2024-11-08 16:27:29 +0530

    update

---
+++ Script.js
@@ -0,0 +1,16 @@
+// var edudiv = document.getElementsByClassName("educont");
+// var visdis = 0;
+
+//         function toggal()
+//         {
+//             if(visdis == 1)
+//             {
+//                 edudiv.style.display = 'block';
+//                 visdis = '0';
+//             }
+
+//             else{
+//                 edudiv.style.display = 'none';
+//                 visdis = '1';
+//             }
+//         }   
